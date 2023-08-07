715c890a5bc4d4ce51e5c696ead050a0e572be92
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-08-07 20:04:51 +0200

    Functional components and props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Todos from '@/components/Todos'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Typescript & React</h1>
+      <Todos items ={['to do 1', 'to do 2', 'to do 3', 'to do 4']}/>
     </main>
   )
 }
